2c03c3e049fa3009d5ec84a546d672e39103b679
Daniel Redly <RedlyDP@cardiff.ac.uk>
2022-05-15 16:53:57 +0100

    Ex6.2

--- src/index.js
+++ src/index.js
@@ -6,21 +6,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const changeState = actionType => {
     store.dispatch({
-      type: 'GOOD'
+      type: actionType
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => changeState('GOOD')}>good</button>
+      <button onClick={() => changeState('OK')}>ok</button>
+      <button onClick={() => changeState('BAD')}>bad</button>
+      <button onClick={() => changeState('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
